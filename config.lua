ba0cbb0472875cce8cda9668a5d746e487e8f335
Walterr399 <walterwhiterp47@gmail.com>
2025-03-16 22:02:03 +0000

    ..

--- config.lua
+++ config.lua
@@ -1,4 +1,4 @@
 Config = {}
 
-Config.Webhook = "" -- It's empty put a webhook in here.
+Config.Webhook = "ADD_YOUR_KENKER_WEBHOOK_HERE" -- It's empty put a webhook in here.
 Config.Color = 16711680, -- Default (red) color.
