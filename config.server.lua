fc82e71ab0c58cb19b5b1a61cf7dbc3297731aec
Walterr399 <walterwhiterp47@gmail.com>
2025-04-25 21:30:54 +0000

    Fixed errors.

--- config.server.lua
+++ config.server.lua
@@ -1,5 +1,4 @@
 Config = {}
 
-Config.Color = 16711680, -- Default (red) color.
-Config.Webhook = "ADD_YOUR_KENKER_WEBHOOK_HERE" -- It's empty put a webhook in here. 
--- Moved webhook to server-sided.
+Config.Color = 16711680 --# red color for example
+Config.Webhook = "ADD_YOUR_WEBHOOK"
--- server.lua
+++ server.lua
@@ -2,12 +2,13 @@ ESX = exports['es_extended']:getSharedObject()
 
 print("^2walter-logging started successfully! Created by: https://github.com/Walterr399^7")
 
+--# Functon
 function sendLog(message, src)
     local identifier = "Anon"
     if src and src ~= 0 then
-        local xWalter = ESX.GetPlayerFromId(src)
-        if xWalter then
-            identifier = xWalter.identifier or "Anon"
+        local xPlayer = ESX.GetPlayerFromId(src)
+        if xPlayer then
+            identifier = xPlayer.identifier
         end        
     end
 
@@ -33,12 +34,6 @@ function sendLog(message, src)
         end
     end, "POST", json.encode(payLoad), { ["Content-Type"] = "application/json" })    
 end
-
-RegisterNetEvent('walter-logging:sendLog')
-AddEventHandler('walter-logging:sendLog', function(message)
-    local src = source
-    sendLog(message, src)
-end)
 
 exports("sendLog", sendLog)
 
